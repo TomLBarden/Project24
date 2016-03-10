24d7e271952581598a7dcf2858402b726ea01e6e
Alex Billington <afbillington96@gmail.com>
2016-03-10 12:46:04 +0000

    AB - Chatroom windows form template, rudimentary format.

---
+++ GossbitBot Chatroom/GossbitBot Chatroom/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GossbitBot_Chatroom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ChatButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(UserNameBox.Text))
+                MessageBox.Show("Please enter your name and try again.");
+
+            else
+            {
+                Program.UserName = UserNameBox.Text;
+
+                Form2 Chatroom = new Form2();
+                Chatroom.ShowDialog();
+            }
+
+
+
+
+        }
+    }
+}
---
+++ GossbitBot Chatroom/GossbitBot Chatroom/Form2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GossbitBot_Chatroom
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+
+            ConversationBox.Items.Add("Marvin: " + "Hi " + Program.UserName + " Nice to meet you. How are you?");
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            //Deals with the users message being sent.
+            //Handles an empty message box.
+            if (string.IsNullOrWhiteSpace(UserMessageBox.Text))
+                MessageBox.Show("Please enter a message to send.");
+
+            //Adds the data in the UserMessageBox to the ConversationBox.
+            ConversationBox.Items.Add(Program.UserName + ": " + UserMessageBox.Text);
+            //Sets the UserMessageBox to being empty.
+            UserMessageBox.Text = null;
+
+
+            // -----    -----
+
+
+            //Deals with the chat rooms reply. 
+            ConversationBox.Items.Add("Marvin: Here is my generic response, I am only a basic AI please love me.");
+        }
+    }
+}
